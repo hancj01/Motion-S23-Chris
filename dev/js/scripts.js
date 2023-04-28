d28c9823657b4673cac0cdeddc17ea878904263d
hancj01 <hanchris1004@gmail.com>
2023-04-27 20:24:05 -0400

    changes

--- dev/js/scripts.js
+++ dev/js/scripts.js
@@ -1,5 +1,11 @@
-//import { gsap } from "gsap";
+import { gsap } from "gsap";
 
 //QUIZ
 
+//Part 1
 
+gsap.timeline()
+
+.from("box red", { duration: 1, x: 0, ease: "power1.out"})
+.from("box blue", {duration: 1, x: 350, ease:"power1.out"})
+;
