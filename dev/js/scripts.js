b48e4d1c966fa77160d7de5ec776cd9b9445e075
hancj01 <hanchris1004@gmail.com>
2023-05-10 23:15:26 -0400

    Experiment

--- dev/js/scripts.js
+++ dev/js/scripts.js
@@ -10,15 +10,15 @@ function heroAnimation(){
     let tl = gsap.timeline({
         scrollTrigger:{
             trigger: "#section-1 h1",
-            start:"top 50%",
-            end:"bottom 40%",
+            start:"top 51%",
+            end:"center 40%",
 
         //start:"top 50%",
         //top/bottom targets the element markers
         //percentage targets the viewport markers
             scrub: true,
             markers:true,
-            toggleActions: "play none"
+            toggleActions: "play none none pause"
         }});
     
         tl.from("#section-1 h1, #section-1 h2",{duration:1, alpha:0, y:100});
@@ -30,4 +30,5 @@ function heroAnimation(){
 
 let mainTl = gsap.timeline();
 mainTl.add(heroAnimation())
+.add
     ;
