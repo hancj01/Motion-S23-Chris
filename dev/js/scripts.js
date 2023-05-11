83c1180831066b6c1f5973aa612e069cde679c10
hancj01 <hanchris1004@gmail.com>
2023-05-10 21:54:14 -0400

    where are the cookies

--- dev/js/scripts.js
+++ dev/js/scripts.js
@@ -1,2 +1,28 @@
 import { gsap } from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
 
+gsap.registerPlugin(ScrollTrigger);
+
+//Start Code
+
+gsap.timeline()
+function heroAnimation(){
+    let tl = gsap.timeline({
+        scrollTrigger:{
+            trigger: "#cookies",
+            start:"top 0%",
+            end:"bottom 50%",
+            scrub: true,
+            markers:true
+        }});
+    
+        tl.from("#cookies",{duration:5, alpha:0.5, y:300});
+        return tl;
+
+
+}
+
+
+let mainTl = gsap.timeline();
+mainTl.add(heroAnimation())
+    ;
