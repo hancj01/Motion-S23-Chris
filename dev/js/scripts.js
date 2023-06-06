d194ba0ca03477bf5cf4008b95da36b9ca9afc10
hancj01 <hanchris1004@gmail.com>
2023-06-06 02:54:39 -0400

    draw

--- dev/js/scripts.js
+++ dev/js/scripts.js
@@ -1,1 +1,27 @@
-//import { gsap } from "gsap";
+import { gsap } from "gsap";
+import { DrawSVGPlugin } from "gsap/DrawSVGPlugin";
+import { GSDevTools } from "gsap/GSDevTools";
+import { MorphSVGPlugin } from "gsap/MorphSVGPlugin";
+import { MotionPathPlugin } from "gsap/MotionPathPlugin";
+
+gsap.registerPlugin(MotionPathPlugin, DrawSVGPlugin, GSDevTools, MorphSVGPlugin);
+
+
+function glassesAnimation(){
+    let tl = gsap.timeline();
+
+    //glasses are drawn
+    tl.from("#green-oval", {drawSVG:"0%", duration: 2, ease: "none", transformOrigin: "top"}, "draw");
+    tl.from("#blue-circle", {drawSVG:"0%", duration: 2, ease: "none", transformOrigin: "top"}, "draw");
+
+    return tl;
+}
+
+
+let mainTL = gsap.timeline();
+mainTL.add(glassesAnimation())
+
+
+;
+
+GSDevTools.create();
