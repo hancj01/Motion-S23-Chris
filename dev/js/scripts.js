eb77619001af0d5b532697834606410360beb98f
hancj01 <hanchris1004@gmail.com>
2023-06-06 00:25:27 -0400

    kinda working

--- dev/js/scripts.js
+++ dev/js/scripts.js
@@ -2,12 +2,43 @@ import { gsap } from "gsap";
 import { DrawSVGPlugin } from "gsap/DrawSVGPlugin";
 import { GSDevTools } from "gsap/GSDevTools";
 import { MorphSVGPlugin } from "gsap/MorphSVGPlugin";
+import { MotionPathPlugin } from "gsap/MotionPathPlugin";
 
-gsap.registerPlugin(DrawSVGPlugin, GSDevTools, MorphSVGPlugin);
+gsap.registerPlugin(MotionPathPlugin, DrawSVGPlugin, GSDevTools, MorphSVGPlugin);
 
-const mainTL = gsap.timeline();
+//INIT
+//gsap.set("#SOHO", {scaleY:0.5});
 
-mainTL.from("soho", {duration:1})
+function blocksAnimation(){
+    let tl = gsap.timeline();
+
+    //entire logo comes down
+    tl.from("#soho", {duration:1, y:"-=500", ease: "none"});
+
+    //blocks split
+    tl.from("#o-1", {duration:0.2, x: "-=175", ease: "none" })
+    tl.from("#H", {duration:0.2, x: "-=345", ease: "none" },"-=0.1")
+    tl.from("#o-2", {duration:0.2, x: "-=515", ease: "none" },"-=0.1")
+
+    return tl;
+}
+
+function textAnimation(){
+    let tl = gsap.timeline();
+
+    tl.from("#redefine", {duration:0.2, x: "-=400", ease: "none"});
+
+    return tl;
+
+}
+
+
+
+let mainTL = gsap.timeline();
+mainTL.add(blocksAnimation())
+mainTL.add(textAnimation())
+
+// mainTL.from("soho", {duration:1, y:50})
 
 
 ;
