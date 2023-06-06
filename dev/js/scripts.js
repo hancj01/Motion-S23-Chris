8cf07053a861eb7a6945e65d130edf0ff7681d02
hancj01 <hanchris1004@gmail.com>
2023-06-06 18:15:56 -0400

    nearly finished yay!

--- dev/js/scripts.js
+++ dev/js/scripts.js
@@ -16,16 +16,16 @@ function blocksAnimation(){
     tl.from("#soho", {duration:1, y:"-=500", ease: "expo.out"});
 
     //blocks split
-    tl.from("#o-1", {duration:0.5, x: "-=175", ease: "none" }, "move+=.05")
+    tl.from("#o-1", {duration:0.5, x: "-=175", ease: "none" }, "move")
     tl.from("#H", {duration:0.5, x: "-=345", ease: "none" },"move+=.05")
     tl.from("#o-2", {duration:0.5, x: "-=515", ease: "none" },"move+=.1")
 
     //yellow blocks grow
-    tl.from("#bottom-s-block", {duration: 0.2, scaleX:0, ease: "none", transformOrigin: "left" }, "grow")
-    tl.from("#top-s-block", {duration: 0.2, scaleX:0, ease: "none", transformOrigin: "right" }, "grow")
-    tl.from("#o-1-block, #o-2-block", {duration: 0.2, scaleX:0, ease: "none", transformOrigin: "center" }, "grow")
-    tl.from("#top-h-block", {duration: 0.2, scaleX:0, ease: "none", transformOrigin: "top" }, "grow")    
-    tl.from("#bottom-h-block", {duration: 0.2, scaleX:0, ease: "none", transformOrigin: "bottom" }, "grow")    
+    tl.from("#bottom-s-block", {duration: 0.2, scaleX:0, ease: "none", transformOrigin: "left" }, "grow+=0.5")
+    tl.from("#top-s-block", {duration: 0.2, scaleX:0, ease: "none", transformOrigin: "right" }, "grow+=0.5")
+    tl.from("#o-1-block, #o-2-block", {duration: 0.2, scaleX:0, ease: "none", transformOrigin: "center" }, "grow+=0.5")
+    tl.from("#top-h-block", {duration: 0.2, scaleX:0, ease: "none", transformOrigin: "top" }, "grow+=0.5")    
+    tl.from("#bottom-h-block", {duration: 0.2, scaleX:0, ease: "none", transformOrigin: "bottom" }, "grow+=0.5")    
 
 
 
