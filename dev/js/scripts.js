82504ec20f6403f5e51f95d1bf973eca9af35884
hancj01 <hanchris1004@gmail.com>
2023-06-06 03:02:06 -0400

    beginning of drawing

--- dev/js/scripts.js
+++ dev/js/scripts.js
@@ -11,16 +11,29 @@ function glassesAnimation(){
     let tl = gsap.timeline();
 
     //glasses are drawn
-    tl.from("#green-oval", {drawSVG:"0%", duration: 2, ease: "none", transformOrigin: "top"}, "draw");
-    tl.from("#blue-circle", {drawSVG:"0%", duration: 2, ease: "none", transformOrigin: "top"}, "draw");
+    tl.from("#green-oval", {drawSVG:"0%", duration: 1, ease: "none", transformOrigin: "top"}, "draw");
+    tl.from("#blue-circle", {drawSVG:"0%", duration: 1, ease: "none", transformOrigin: "top"}, "draw");
+
+    //
 
     return tl;
 }
 
+function faceAnimation(){
+    let tl = gsap.timeline();
+
+    //glasses are drawn
+    tl.from("#right-eye", {duration: 2, scaleX:0, ease: "elastic.out", transformOrigin: "center"}, "open");
+    tl.from("#left-eye", {duration: 2, scaleX:0, ease: "elastic.out", transformOrigin: "center"}, "open");
+
+    return tl;
+}
+
+//tl.from("#bottom-s-block", {duration: 0.2, scaleX:0, ease: "none", transformOrigin: "left" }, "grow")
 
 let mainTL = gsap.timeline();
 mainTL.add(glassesAnimation())
-
+mainTL.add(faceAnimation())
 
 ;
 
