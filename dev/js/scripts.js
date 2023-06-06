cb53ebb9b0b4f8491f09ff5ebe8f127c030aa319
hancj01 <hanchris1004@gmail.com>
2023-06-06 01:48:55 -0400

    nearly there!

--- dev/js/scripts.js
+++ dev/js/scripts.js
@@ -13,15 +13,21 @@ function blocksAnimation(){
     let tl = gsap.timeline();
 
     //entire logo comes down
-    tl.from("#soho", {duration:1, y:"-=500", ease: "none"});
+    tl.from("#soho", {duration:1, y:"-=500", ease: "expo.out"});
 
     //blocks split
-    tl.from("#o-1", {duration:0.4, x: "-=175", ease: "none" }, "move")
-    tl.from("#H", {duration:0.4, x: "-=345", ease: "none" },"move+=.05")
-    tl.from("#o-2", {duration:0.4, x: "-=515", ease: "none" },"move+=.1")
+    tl.from("#o-1", {duration:0.5, x: "-=175", ease: "none" }, "move")
+    tl.from("#H", {duration:0.5, x: "-=345", ease: "none" },"move+=.05")
+    tl.from("#o-2", {duration:0.5, x: "-=515", ease: "none" },"move+=.1")
 
     //yellow blocks grow
-    tl.from("bottom-s-block", {duration: 0.2})
+    tl.from("#bottom-s-block", {duration: 0.2, scaleX:0, ease: "none", transformOrigin: "left" }, "grow")
+    tl.from("#top-s-block", {duration: 0.2, scaleX:0, ease: "none", transformOrigin: "right" }, "grow")
+    tl.from("#o-1-block, #o-2-block", {duration: 0.2, scaleX:0, ease: "none", transformOrigin: "center" }, "grow")
+    tl.from("#top-h-block", {duration: 0.2, scaleX:0, ease: "none", transformOrigin: "top" }, "grow")    
+    tl.from("#bottom-h-block", {duration: 0.2, scaleX:0, ease: "none", transformOrigin: "bottom" }, "grow")    
+
+
 
 
     return tl;
@@ -30,10 +36,10 @@ function blocksAnimation(){
 function splitAnimation(){
     let tl = gsap.timeline();
 
-    tl.from("#redefine", {duration:0.2, x: "-=400", ease: "none"}, "split");
+    tl.from("#redefine", {duration:0.3, x: "-=400", ease: "none"}, "split+=0.5");
     
     //soho moves to the left
-    tl.from("#soho", {duration:0.2, x: "+=175", ease: "none" }, "split")
+    tl.from("#soho", {duration:0.3, x: "+=175", ease: "none" }, "split+=0.5")
 
     return tl;
 }
