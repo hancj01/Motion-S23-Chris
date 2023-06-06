5f891123a1db71d875a3098c31fa0b13b83e17d4
hancj01 <hanchris1004@gmail.com>
2023-06-06 00:53:42 -0400

    motion!

--- dev/js/scripts.js
+++ dev/js/scripts.js
@@ -16,27 +16,33 @@ function blocksAnimation(){
     tl.from("#soho", {duration:1, y:"-=500", ease: "none"});
 
     //blocks split
-    tl.from("#o-1", {duration:0.2, x: "-=175", ease: "none" })
-    tl.from("#H", {duration:0.2, x: "-=345", ease: "none" },"-=0.1")
-    tl.from("#o-2", {duration:0.2, x: "-=515", ease: "none" },"-=0.1")
+    tl.from("#o-1", {duration:0.4, x: "-=175", ease: "none" }, "move")
+    tl.from("#H", {duration:0.4, x: "-=345", ease: "none" },"move+=.05")
+    tl.from("#o-2", {duration:0.4, x: "-=515", ease: "none" },"move+=.1")
+
+    //yellow blocks grow
+    tl.from("bottom-s-block", {duration: 0.2})
+
 
     return tl;
 }
 
-function textAnimation(){
+function splitAnimation(){
     let tl = gsap.timeline();
 
-    tl.from("#redefine", {duration:0.2, x: "-=400", ease: "none"});
+    tl.from("#redefine", {duration:0.2, x: "-=400", ease: "none"}, "split");
+    
+    //soho moves to the left
+    tl.from("#soho", {duration:0.2, x: "+=175", ease: "none" }, "split")
 
     return tl;
-
 }
 
 
 
 let mainTL = gsap.timeline();
 mainTL.add(blocksAnimation())
-mainTL.add(textAnimation())
+mainTL.add(splitAnimation())
 
 // mainTL.from("soho", {duration:1, y:50})
 
