732ab42b96ab7bf50789d61d430abb71d6a4fe1c
hancj01 <hanchris1004@gmail.com>
2023-06-07 22:31:41 -0400

    completed animation

--- dev/js/scripts.js
+++ dev/js/scripts.js
@@ -11,8 +11,8 @@ function glassesAnimation(){
     let tl = gsap.timeline();
 
     //glasses are drawn
-    tl.from("#green-oval", {drawSVG:"0%", duration: 1, ease: "none", transformOrigin: "100% 50%"}, "draw");
-    tl.from("#blue-circle", {drawSVG:"0%", duration: 1, ease: "none", transformOrigin: "top"}, "draw");
+    tl.from("#green-oval", {drawSVG:"0%", duration: 1, ease: "none"}, "draw");
+    tl.from("#blue-circle", {drawSVG:"0%", duration: 1, ease: "none"}, "draw");
 
     //glasses arms are drawn
     tl.from("#green-arm", {drawSVG:"0%", duration: 0.7, ease: "none", transformOrigin: "left"}, "draw2");
@@ -20,22 +20,42 @@ function glassesAnimation(){
     return tl;
 }
 
-function faceAnimation(){
+function eyesAnimation(){
     let tl = gsap.timeline();
 
     //eyes appear
-    tl.from("#right-eye", {duration: 2, scaleY:0, ease: "elastic.out", transformOrigin: "center"}, "open");
-    tl.from("#left-eye", {duration: 2, scaleY:0, ease: "elastic.out", transformOrigin: "center"}, "open");
+    tl.from("#right-eye", {duration: 1, scaleY:0, ease: "elastic.out(1, 0.5)", transformOrigin: "center"}, "open");
+    tl.from("#left-eye", {duration: 1, scaleY:0, ease: "elastic.out(1, 0.5)", transformOrigin: "center"}, "open");
 
     return tl;
 }
+
+function smileAnimation(){
+    let tl = gsap.timeline();
+
+    gsap.set("#smile-all", { transformOrigin: "top" });
+
+    //smile swings in
+    tl.from("#smile-all", {duration:0.75, rotate:135, ease:"elastic.out(.7, 1)" });
+
+    return tl;
+}
+
+function blinkAnimation(){
+    let tl = gsap.timeline({ repeat: 3, yoyo: true });
+
+    tl.to("#eyes", { scaleY: 0, duration: .2, ease: "power1.inOut", transformOrigin: "center"});
+    return tl;
+}
+
 
 //tl.from("#bottom-s-block", {duration: 0.2, scaleX:0, ease: "none", transformOrigin: "left" }, "grow")
 
 let mainTL = gsap.timeline();
 mainTL.add(glassesAnimation())
-mainTL.add(faceAnimation())
-
+mainTL.add(eyesAnimation())
+mainTL.add(smileAnimation())
+mainTL.add(blinkAnimation())
 ;
 
 GSDevTools.create();
